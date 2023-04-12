8dd6c082a812dad07952dfe11af03ab2b5d51ed5
RuvenBeck <69506858+RuvenBeck@users.noreply.github.com>
2023-04-12 14:48:52 +0200

    Update streaming.py
    Bug Fixes, more utility

--- streaming.py
+++ streaming.py
@@ -13,21 +13,33 @@ from seleniumwire import webdriver
 import threading
 from selenium import webdriver
 import queue
+from pyfiglet import Figlet
+from colorama import Fore, Back, Style
 
-count = 0
+
+custom_fig = Figlet(font='graffiti')
+print(custom_fig.renderText('Legendary Streamer'))
+print(rf"""{Fore.RED}                                                      v1
+                                                    by RuvenBeck   
+{Style.RESET_ALL}
+""")
+
+lock = threading.Lock()
+
 
 with open('proxies.txt', 'r') as f:
     proxies = f.read().splitlines()
 chrome_options = Options()
 chrome_options.add_argument('--proxy-server=%s' % proxies[0])
+chrome_options.add_argument('--disable-webrtc')
 chrome_options.add_argument('--start-maximized')
 
 with open('Playlist.txt', 'r') as p:
     playlist_urls = p.readlines()
 
 
+def worker(q, chrome_options,):
 
-def worker(q, chrome_options):
     while True:
         line = q.get()
         if line is None:
@@ -40,7 +52,7 @@ def worker(q, chrome_options):
 
         driver = webdriver.Chrome(options=chrome_options)
         driver.get('https://accounts.spotify.com/de/login?continue=https%3A%2F%2Fopen.spotify.com%2F__noul__%3Fl2l%3D1%26nd%3D1&_locale=de-DE')
-        #driver.maximize_window()
+
         WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.ID, 'login-username'))).send_keys(email)
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'login-password'))).send_keys(password)
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID,'login-button'))).click()
@@ -65,23 +77,42 @@ def worker(q, chrome_options):
             playlist_url = random.choice(playlist_urls)        
             driver.get(playlist_url)
         
-        #Play
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main"]/div/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div[2]/main/div[1]/section/div[2]/div[2]/div[4]/div/div/div/div/div/button/span'))).click()
 
         #Loop und Shuffle an machen
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'Vz6yjzttS0YlLcwrkoUR'))).click()
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "KVKoQ3u4JpKTvSSFtd6J"))).click()
+        loopactive = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'Vz6yjzttS0YlLcwrkoUR')))
+        shuffleactive = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'KVKoQ3u4JpKTvSSFtd6J')))
+
+        if loopactive.get_attribute("aria-checked") == "true":
+            print("Loop Bereits aktiviert")
+        else:
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'Vz6yjzttS0YlLcwrkoUR'))).click()
+
+        if loopactive.get_attribute("aria-checked") == "mixed":
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'Vz6yjzttS0YlLcwrkoUR'))).click()
+            time.sleep(0.5)
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'Vz6yjzttS0YlLcwrkoUR'))).click()
+            print("Dauerhafter-Loop auf Loop gestellt.")
+
+        else:
+            print("Alles bereits richtig")
+
+        if shuffleactive.get_attribute("aria-checked") == "true":
+            print("Shuffle bereits aktiviert")
+        else:
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'KVKoQ3u4JpKTvSSFtd6J'))).click()
+
         count = 0
-    
+
         while True:
             count += 1
+
 
             driver.find_element(By.CLASS_NAME, 'mnipjT4SLDMgwiDCEnRC').click()
 
 
             print(f"Die Songs wurden {count} mal gestreamt!")
 
-            if count == 400:
+            if count == 1000:
                 driver.quit()
                 break
 
@@ -95,6 +126,7 @@ def worker(q, chrome_options):
             print(f"Der Song wurde {duration} Sekunden gespielt.")
         q.task_done()
 
+
 q = queue.Queue()
 
 # Zeilen aus der Textdatei in die Queue einfügen
@@ -103,9 +135,15 @@ with open('combo.txt', 'r') as file:
     for line in lines:
         q.put(line)
 
+
+with open('combo.txt') as f:
+    anzahl_zeilen = sum(1 for line in f)
+
+anzahl_threads = anzahl_zeilen
+
 # Threads erstellen
 threads = []
-for i in range(8):
+for i in range(anzahl_threads):
     t = threading.Thread(target=worker, args=(q, chrome_options))
     t.start()
     threads.append(t)
@@ -114,7 +152,8 @@ for i in range(8):
 q.join()
 
 # Alle Threads beenden
-for i in range(8):
+for i in range(anzahl_threads):
     q.put(None)
 for t in threads:
     t.join()
+
